7c30523629f4982df4d8206ef9c26e03cfaacf0b
Vishnu Sankar <iamvishnusankar@live.com>
2020-08-03 00:46:30 +0530

    - WIP

--- packages/next-sitemap/src/buildSitemapXml/index.ts
+++ packages/next-sitemap/src/buildSitemapXml/index.ts
@@ -1,20 +1,11 @@
 import { IConfig } from '../interface'
-import { prettyXml } from '../pretty'
 
 export const buildSitemapXml = (config: IConfig, urls: string[]) => {
   const urlArr = urls.reduce(
-    (prev, curr) => `${prev}
-    <url>
-    <loc>${curr}</loc>
-    <changefreq>${config.changefreq}</changefreq>
-    <priority>${config.priority}</priority>
-  </url>`,
+    (prev, curr) =>
+      `${prev}<url><loc>${curr}</loc><changefreq>${config.changefreq}</changefreq><priority>${config.priority}</priority></url>\n`,
     ''
   )
 
-  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>\n\t<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-    ${urlArr}
-    </urlset>`
-
-  return prettyXml(sitemapXml, '  ')
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${urlArr}</urlset>`
 }
