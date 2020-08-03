9f960bde583eca8e9bfc2c389ef6da672620a46b
Vishnu Sankar <iamvishnusankar@live.com>
2020-08-03 12:07:33 +0530

    - Added support for splitting sitemap

--- packages/next-sitemap/src/buildSitemapXml/index.ts
+++ packages/next-sitemap/src/buildSitemapXml/index.ts
@@ -1,11 +1,15 @@
 import { IConfig } from '../interface'
 
+export const withXMLTemplate = (content: string) => {
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${content}</urlset>`
+}
+
 export const buildSitemapXml = (config: IConfig, urls: string[]) => {
-  const urlArr = urls.reduce(
+  const content = urls.reduce(
     (prev, curr) =>
       `${prev}<url><loc>${curr}</loc><changefreq>${config.changefreq}</changefreq><priority>${config.priority}</priority></url>\n`,
     ''
   )
 
-  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${urlArr}</urlset>`
+  return withXMLTemplate(content)
 }
--- packages/next-sitemap/src/url/index.ts
+++ packages/next-sitemap/src/url/index.ts
@@ -13,17 +13,28 @@ export const generateUrl = (baseUrl: string, slug: string) => {
   return isURL(slug) ? cleanPath(slug) : cleanPath(`${baseUrl}/${slug}`)
 }
 
+/**
+ * Create a unique url set
+ * @param config
+ * @param manifest
+ */
 export const createUrlSet = (config: IConfig, manifest: INextManifest) => {
   const allKeys = [
     ...Object.keys(manifest.build.pages),
     ...(manifest.preRender ? Object.keys(manifest.preRender.routes) : [])
   ]
 
-  return new Set(
+  const urlSet = new Set(
     allKeys.flatMap((x) => (!isNextInternalUrl(x) ? generateUrl(config.siteUrl, x) : []))
   )
+
+  return [...urlSet]
 }
 
+/**
+ * Checks whether a url is next.js specific or not
+ * @param path path check
+ */
 export const isNextInternalUrl = (path: string) => {
   return new RegExp(/[^\/]*[_\[]+(.*)/g).test(path)
 }
