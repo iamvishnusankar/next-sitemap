c45785c51d00162500421da4b5a11ad8e299f8aa
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2020-08-23 23:20:51 +0530

    - WIP

---
+++ packages/next-sitemap/src/url/util/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-useless-escape */
+
+export const cleanPath = (text: string): string => {
+  return text.replace(/([^:])(\/\/+)/g, '$1/')
+}
+
+export const isURL = (text: string): boolean => {
+  const regexp = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+  return regexp.test(text)
+}
+
+export const generateUrl = (baseUrl: string, slug: string): string => {
+  return isURL(slug) ? cleanPath(slug) : cleanPath(`${baseUrl}/${slug}`)
+}
+
+/**
+ * Checks whether a url is next.js specific or not
+ * @param path path check
+ */
+export const isNextInternalUrl = (path: string): boolean => {
+  return new RegExp(/[^\/]*[_\[]+(.*)/g).test(path)
+}
