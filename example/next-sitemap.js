7dbd8fdd25909997ce66918143d192d2ad5a489f
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2020-09-02 18:01:36 +0530

    Change trasnform property url to loc

--- example/next-sitemap.js
+++ example/next-sitemap.js
@@ -2,13 +2,13 @@ module.exports = {
   siteUrl: 'https://example.com',
   generateRobotsTxt: true,
   // Optional custom transformation function
-  transform: (_, url) => {
-    return {
-      url,
-      changefreq: 'yearly',
-      custom: 'prop',
-    }
-  },
+  // transform: (_, path) => {
+  //   return {
+  //     loc: path,
+  //     // changefreq: 'yearly',
+  //     custom: 'prop',
+  //   }
+  // },
   // optional
   robotsTxtOptions: {
     additionalSitemaps: [
