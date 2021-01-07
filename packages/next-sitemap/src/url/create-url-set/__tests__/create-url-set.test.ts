87f9f7b68ff1843650936b99e31a66a828529540
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2021-01-07 23:33:04 +0530

    Fix: #82

--- packages/next-sitemap/src/url/create-url-set/__tests__/create-url-set.test.ts
+++ packages/next-sitemap/src/url/create-url-set/__tests__/create-url-set.test.ts
@@ -1,8 +1,8 @@
-import { createUrlSet } from '.'
-import { sampleConfig } from '../../fixtures/config'
-import { sampleManifest } from '../../fixtures/manifest'
+import { createUrlSet } from '..'
+import { sampleConfig } from '../../../fixtures/config'
+import { sampleManifest } from '../../../fixtures/manifest'
 
-describe('next-sitemap/createUrlSet', () => {
+describe('createUrlSet', () => {
   test('without exclusion', () => {
     const urlset = createUrlSet(sampleConfig, sampleManifest)
     expect(urlset).toStrictEqual([
@@ -10,7 +10,7 @@ describe('next-sitemap/createUrlSet', () => {
         changefreq: 'daily',
         lastmod: expect.any(String),
         priority: 0.7,
-        loc: 'https://example.com/',
+        loc: 'https://example.com',
       },
       {
         changefreq: 'daily',
@@ -78,7 +78,49 @@ describe('next-sitemap/createUrlSet', () => {
         changefreq: 'daily',
         lastmod: expect.any(String),
         priority: 0.7,
-        loc: 'https://example.com/',
+        loc: 'https://example.com',
+      },
+    ])
+  })
+
+  test('without trailing slash', () => {
+    const urlset = createUrlSet(
+      {
+        ...sampleConfig,
+        trailingSlash: false,
+      },
+      sampleManifest
+    )
+    expect(urlset).toStrictEqual([
+      {
+        changefreq: 'daily',
+        lastmod: expect.any(String),
+        priority: 0.7,
+        loc: 'https://example.com',
+      },
+      {
+        changefreq: 'daily',
+        lastmod: expect.any(String),
+        priority: 0.7,
+        loc: 'https://example.com/page-0',
+      },
+      {
+        changefreq: 'daily',
+        lastmod: expect.any(String),
+        priority: 0.7,
+        loc: 'https://example.com/page-1',
+      },
+      {
+        changefreq: 'daily',
+        lastmod: expect.any(String),
+        priority: 0.7,
+        loc: 'https://example.com/page-2',
+      },
+      {
+        changefreq: 'daily',
+        lastmod: expect.any(String),
+        priority: 0.7,
+        loc: 'https://example.com/page-3',
       },
     ])
   })
