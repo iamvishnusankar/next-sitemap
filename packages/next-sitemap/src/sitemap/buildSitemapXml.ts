9c0e9f13d96bcbdaf1b5cce007c4aa16ee6f749e
Ethan Standel <ethan.standel@rocketinsights.com>
2021-05-24 16:12:19 -0400

    Improve XML builder efficiency for sitemap XML.

--- packages/next-sitemap/src/sitemap/buildSitemapXml.ts
+++ packages/next-sitemap/src/sitemap/buildSitemapXml.ts
@@ -2,19 +2,19 @@ import { ISitemapFiled } from '../interface'
 import { withXMLTemplate } from './withXMLTemplate'
 
 export const buildSitemapXml = (fields: ISitemapFiled[]): string => {
-  const content = fields.reduce((prev, curr) => {
-    let field = ''
+  const content = fields.map(fieldData => {
+    const field: Array<string> = [];
 
     // Iterate all object keys and key value pair to field-set
-    for (const key of Object.keys(curr)) {
-      if (curr[key]) {
-        field += `<${key}>${curr[key]}</${key}>`
+    for (const key of Object.keys(fieldData)) {
+      if (fieldData[key]) {
+        field.push(`<${key}>${fieldData[key]}</${key}>`);
       }
     }
 
     // Append previous value and return
-    return `${prev}<url>${field}</url>\n`
-  }, '')
+    return `<url>${field.join("")}</url>\n`
+  }).join("");
 
   return withXMLTemplate(content)
 }
