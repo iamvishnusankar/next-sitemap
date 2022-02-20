5ee0cc52a948748c24f6049794a5fc2b9a3ab36d
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2022-02-20 21:34:10 +0530

    Added AMP pages support
    Fix: #198

---
+++ examples/amp/pages/about.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>AboutPage Component</h1>
+    </div>
+  )
+}
+
+export default AboutPage
+
+// https://nextjs.org/docs/api-reference/next/amp
+export const config = {
+  amp: true,
+}
