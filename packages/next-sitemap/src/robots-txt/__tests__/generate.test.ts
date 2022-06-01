8b1a324487f4e24ae5cd1450f54a272ca239fd10
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2022-06-01 20:00:26 +0530

    Added UrlSetBuilder.createUrlSet tests

--- packages/next-sitemap/src/robots-txt/__tests__/generate.test.ts
+++ packages/next-sitemap/src/robots-txt/__tests__/generate.test.ts
@@ -12,6 +12,29 @@ describe('next-sitemap/generateRobotsTxt', () => {
   })
 
   test('generateRobotsTxt: additionalSitemap', () => {
-    expect(generateRobotsTxt(sampleConfig as any)).toMatchInlineSnapshot()
+    expect(generateRobotsTxt(sampleConfig as any)).toMatchInlineSnapshot(`
+      "# *
+      User-agent: *
+      Allow: /
+
+      # black-listed-bot
+      User-agent: black-listed-bot
+      Disallow: /sub-path-1
+      Disallow: /path-2
+
+      # friendly-bot
+      User-agent: friendly-bot
+      Allow: /
+      Crawl-delay: 10
+
+      # Host
+      Host: https://example.com
+
+      # Sitemaps
+      Sitemap: https://example.com/my-custom-sitemap-1.xml
+      Sitemap: https://example.com/my-custom-sitemap-2.xml
+      Sitemap: https://example.com/my-custom-sitemap-3.xml
+      "
+    `)
   })
 })
