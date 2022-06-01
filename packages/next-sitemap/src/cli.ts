87093de29e422f7f41f9bcf4fe50998b548a6e2a
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2022-06-01 20:58:17 +0530

    Fix bin files

--- packages/next-sitemap/src/cli.ts
+++ packages/next-sitemap/src/cli.ts
@@ -7,66 +7,76 @@ import { ConfigParser } from './parsers/config-parser.js'
 import { ManifestParser } from './parsers/manifest-parser.js'
 import { UrlSetBuilder } from './builders/url-set-builder.js'
 
-// Async main
-const main = async () => {
-  // Create config parser instance
-  const configParser = new ConfigParser()
+export class CLI {
+  /**
+   * Main method
+   * @returns
+   */
+  async main() {
+    // Create config parser instance
+    const configParser = new ConfigParser()
 
-  // Load base config from `next-sitemap.config.js`
-  let config = await configParser.loadBaseConfig()
+    // Load base config from `next-sitemap.config.js`
+    let config = await configParser.loadBaseConfig()
 
-  // Find the runtime paths using base config
-  const runtimePaths = getRuntimePaths(config)
+    // Find the runtime paths using base config
+    const runtimePaths = getRuntimePaths(config)
 
-  // Update base config with runtime config
-  config = await configParser.withRuntimeConfig(config, runtimePaths)
+    // Update base config with runtime config
+    config = await configParser.withRuntimeConfig(config, runtimePaths)
 
-  // Create next.js manifest parser instance
-  const manifestParser = new ManifestParser()
+    // Create next.js manifest parser instance
+    const manifestParser = new ManifestParser()
 
-  // Load next.js manifest
-  const manifest = await manifestParser.loadManifest(runtimePaths)
+    // Load next.js manifest
+    const manifest = await manifestParser.loadManifest(runtimePaths)
 
-  // Create UrlSetBuilder instance
-  const urlSetBuilder = new UrlSetBuilder(config, manifest)
+    // Create UrlSetBuilder instance
+    const urlSetBuilder = new UrlSetBuilder(config, manifest)
 
-  // Generate url set
-  const urlSet = await urlSetBuilder.createUrlSet()
+    // Generate url set
+    const urlSet = await urlSetBuilder.createUrlSet()
 
-  // Split sitemap into multiple files
-  const chunks = toChunks(urlSet, config.sitemapSize!)
+    // Split sitemap into multiple files
+    const chunks = toChunks(urlSet, config.sitemapSize!)
 
-  // All sitemaps array to keep track of generated sitemap files.
-  // Later to be added on robots.txt
-  const generatedSitemaps: string[] = []
+    // All sitemaps array to keep track of generated sitemap files.
+    // Later to be added on robots.txt
+    const generatedSitemaps: string[] = []
 
-  // Generate sitemaps from chunks
-  // await Promise.all(
-  //   sitemapChunks.map(async (chunk) => {
-  //     // Get sitemap absolute url
-  //     const sitemapUrl = generateUrl(config.siteUrl, `/${chunk.filename}`)
+    // Generate sitemaps from chunks
+    // await Promise.all(
+    //   sitemapChunks.map(async (chunk) => {
+    //     // Get sitemap absolute url
+    //     const sitemapUrl = generateUrl(config.siteUrl, `/${chunk.filename}`)
 
-  //     // Add generate sitemap to sitemap list
-  //     generatedSitemaps.push(sitemapUrl)
+    //     // Add generate sitemap to sitemap list
+    //     generatedSitemaps.push(sitemapUrl)
 
-  //     // Generate sitemap
-  //     return generateSitemap(chunk)
-  //   })
-  // )
+    //     // Generate sitemap
+    //     return generateSitemap(chunk)
+    //   })
+    // )
 
-  // Create result object
-  const result: INextSitemapResult = {
-    runtimePaths,
-    generatedSitemaps,
+    // Create result object
+    const result: INextSitemapResult = {
+      runtimePaths,
+      generatedSitemaps,
+    }
+
+    // Generate robots.txt
+    if (config?.generateRobotsTxt) {
+      //  await exportRobotsTxt(config, result)
+    }
+
+    return result
   }
 
-  // Generate robots.txt
-  if (config?.generateRobotsTxt) {
-    //  await exportRobotsTxt(config, result)
-  }
+  async execute() {
+    // Run main method
+    const result = await this.main()
 
-  return result
+    // Log result
+    Logger.generationCompleted(result)
+  }
 }
-
-// Execute
-main().then(Logger.generationCompleted)
