98756dd9a2fe375fc1fd1bf482827cf97da67f87
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2022-06-02 13:03:26 +0530

    Remove bin file generation to simplify CI

--- packages/next-sitemap/bin/next-sitemap.mjs
+++ packages/next-sitemap/bin/next-sitemap.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import envLoader from '@next/env'
-import { CLI } from './cli.js'
+import { CLI } from '../dist/esm/cli.js'
 
 // Load environment variables
 envLoader.loadEnvConfig(process.cwd(), process.env.NODE_ENV === 'development')
