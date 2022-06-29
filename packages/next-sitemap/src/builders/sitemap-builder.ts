770cb676dc1789a723d11af8a9ba5413d04b09ca
akamfoad <akamfanaye@gmail.com>
2022-06-29 19:12:41 +0300

    rm: unnecessary spaces in generated `sitemap.xml`

--- packages/next-sitemap/src/builders/sitemap-builder.ts
+++ packages/next-sitemap/src/builders/sitemap-builder.ts
@@ -21,11 +21,9 @@ export class SitemapBuilder {
    */
   buildSitemapIndexXml(allSitemaps: string[]) {
     return `<?xml version="1.0" encoding="UTF-8"?>
-        <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${allSitemaps
-          ?.map((x) => `<sitemap><loc>${x}</loc></sitemap>`)
-          .join('\n')}
-        </sitemapindex>`
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allSitemaps?.map((x) => `<sitemap><loc>${x}</loc></sitemap>`).join('\n')}
+</sitemapindex>`
   }
 
   /**
