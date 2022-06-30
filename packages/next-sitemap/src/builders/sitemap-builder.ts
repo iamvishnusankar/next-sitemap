7e197fcd9d1d5b434fc920b3e58f19b79224cf10
akamfoad <akamfanaye@gmail.com>
2022-06-30 11:22:27 +0300

    fix: code style

--- packages/next-sitemap/src/builders/sitemap-builder.ts
+++ packages/next-sitemap/src/builders/sitemap-builder.ts
@@ -20,10 +20,12 @@ export class SitemapBuilder {
    * @returns
    */
   buildSitemapIndexXml(allSitemaps: string[]) {
-    return `<?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${allSitemaps?.map((x) => `<sitemap><loc>${x}</loc></sitemap>`).join('\n')}
-</sitemapindex>`
+    return [
+      '<?xml version="1.0" encoding="UTF-8"?>',
+      '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+      allSitemaps?.map((x) => `<sitemap><loc>${x}</loc></sitemap>`).join('\n'),
+      '</sitemapindex>',
+    ].join('\n')
   }
 
   /**
