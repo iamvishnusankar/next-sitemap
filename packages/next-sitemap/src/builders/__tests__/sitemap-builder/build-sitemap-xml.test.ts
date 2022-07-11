42421f278b6dbab6c38abe95005fc2a489bc4930
Kristian Zuffa <kristian.zuffa@gmail.com>
2022-07-11 13:57:06 +0200

    updated docs ; updated date format to match specs in google docs

--- packages/next-sitemap/src/builders/__tests__/sitemap-builder/build-sitemap-xml.test.ts
+++ packages/next-sitemap/src/builders/__tests__/sitemap-builder/build-sitemap-xml.test.ts
@@ -46,7 +46,7 @@ describe('SitemapBuilder', () => {
         loc: 'https://example.com',
         news: {
           title: 'Companies A, B in Merger Talks',
-          date: new Date(2008, 0, 2),
+          date: new Date('2008-01-02T00:00:00.000+01:00'),
           publicationLanguage: 'en',
           publicationName: 'The Example Times',
         },
@@ -57,7 +57,7 @@ describe('SitemapBuilder', () => {
     expect(content).toMatchInlineSnapshot(`
       "<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?>
       <urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\" xmlns:xhtml=\\"http://www.w3.org/1999/xhtml\\" xmlns:mobile=\\"http://www.google.com/schemas/sitemap-mobile/1.0\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xmlns:video=\\"http://www.google.com/schemas/sitemap-video/1.1\\">
-      <url><loc>https://example.com</loc><news:news><news:publication><news:name>The Example Times</news:name><news:language>en</news:language></news:publication><news:publication_date>2008-01-01T23:00:00.000Z</news:publication_date><news:title>Companies A&#44; B in Merger Talks</news:title></news:news></url>
+      <url><loc>https://example.com</loc><news:news><news:publication><news:name>The Example Times</news:name><news:language>en</news:language></news:publication><news:publication_date>2008-01-02T00:00:00.000+01:00</news:publication_date><news:title>Companies A&#44; B in Merger Talks</news:title></news:news></url>
       </urlset>"
     `)
   })
@@ -114,7 +114,7 @@ describe('SitemapBuilder', () => {
               'Alkis shows you how to get perfectly done steaks every time',
             thumbnailLoc: new URL('https://example.com'),
             duration: 600,
-            expirationDate: new Date(2030, 2, 2),
+            expirationDate: new Date('2030-03-02T00:00:00.000+01:00'),
             familyFriendly: true,
             live: false,
             platform: {
@@ -122,7 +122,7 @@ describe('SitemapBuilder', () => {
               content: 'web',
             },
             playerLoc: new URL('https://example.com'),
-            publicationDate: new Date(2020, 3, 20),
+            publicationDate: new Date('2020-04-20T00:00:00.000+02:00'),
             rating: 1,
             requiresSubscription: false,
             restriction: {
@@ -144,7 +144,7 @@ describe('SitemapBuilder', () => {
     expect(content).toMatchInlineSnapshot(`
       "<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?>
       <urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\" xmlns:xhtml=\\"http://www.w3.org/1999/xhtml\\" xmlns:mobile=\\"http://www.google.com/schemas/sitemap-mobile/1.0\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xmlns:video=\\"http://www.google.com/schemas/sitemap-video/1.1\\">
-      <url><loc>https://example.com</loc><video:video><video:title>Video title</video:title><video:thumbnail_loc>https://example.com/</video:thumbnail_loc><video:description>Video description</video:description><video:content_loc>https://example.com/</video:content_loc></video:video><video:video><video:title>Grilling steaks for summer</video:title><video:thumbnail_loc>https://example.com/</video:thumbnail_loc><video:description>Alkis shows you how to get perfectly done steaks every time</video:description><video:content_loc>https://example.com/</video:content_loc><video:player_loc>https://example.com/</video:player_loc><video:duration>600</video:duration><video:view_count>1234</video:view_count><video:tag>video</video:tag><video:rating>1.0</video:rating><video:expiration_date>2030-03-01T23:00:00.000Z</video:expiration_date><video:publication_date>2020-04-19T22:00:00.000Z</video:publication_date><video:family_friendly>yes</video:family_friendly><video:requires_subscription>no</video:requires_subscription><video:live>no</video:live><video:restriction relationship=\\"deny\\">CZ</video:restriction><video:platform relationship=\\"allow\\">web</video:platform><video:uploader info=\\"https://example.com/\\">John Doe</video:uploader></video:video></url>
+      <url><loc>https://example.com</loc><video:video><video:title>Video title</video:title><video:thumbnail_loc>https://example.com/</video:thumbnail_loc><video:description>Video description</video:description><video:content_loc>https://example.com/</video:content_loc></video:video><video:video><video:title>Grilling steaks for summer</video:title><video:thumbnail_loc>https://example.com/</video:thumbnail_loc><video:description>Alkis shows you how to get perfectly done steaks every time</video:description><video:content_loc>https://example.com/</video:content_loc><video:player_loc>https://example.com/</video:player_loc><video:duration>600</video:duration><video:view_count>1234</video:view_count><video:tag>video</video:tag><video:rating>1.0</video:rating><video:expiration_date>2030-03-02T00:00:00.000+01:00</video:expiration_date><video:publication_date>2020-04-20T00:00:00.000+02:00</video:publication_date><video:family_friendly>yes</video:family_friendly><video:requires_subscription>no</video:requires_subscription><video:live>no</video:live><video:restriction relationship=\\"deny\\">CZ</video:restriction><video:platform relationship=\\"allow\\">web</video:platform><video:uploader info=\\"https://example.com/\\">John Doe</video:uploader></video:video></url>
       </urlset>"
     `)
   })
