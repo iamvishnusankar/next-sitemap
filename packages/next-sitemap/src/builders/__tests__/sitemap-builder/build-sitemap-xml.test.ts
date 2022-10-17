4f87ba7afebeb1f0e422f216a97ee678e21683d6
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2022-10-17 19:34:35 +0530

    Fix snapshot test

--- packages/next-sitemap/src/builders/__tests__/sitemap-builder/build-sitemap-xml.test.ts
+++ packages/next-sitemap/src/builders/__tests__/sitemap-builder/build-sitemap-xml.test.ts
@@ -30,7 +30,7 @@ describe('SitemapBuilder', () => {
     // Expect the generated sitemap to match snapshot.
     expect(content).toMatchInlineSnapshot(`
       "<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?>
-      <urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\" xmlns:xhtml=\\"http://www.w3.org/1999/xhtml\\" xmlns:mobile=\\"http://www.google.com/schemas/sitemap-mobile/1.0\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xmlns:video=\\"http://www.google.com/schemas/sitemap-video/1.1\\">
+      <urlset xmlns=\\"https://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"https://www.google.com/schemas/sitemap-news/0.9\\" xmlns:xhtml=\\"https://www.w3.org/1999/xhtml\\" xmlns:mobile=\\"https://www.google.com/schemas/sitemap-mobile/1.0\\" xmlns:image=\\"https://www.google.com/schemas/sitemap-image/1.1\\" xmlns:video=\\"https://www.google.com/schemas/sitemap-video/1.1\\">
       <url><loc>https://example.com</loc></url>
       <url><loc>https://example.com</loc><lastmod>some-value</lastmod><xhtml:link rel=\\"alternate\\" hreflang=\\"en\\" href=\\"https://example.com/en\\"/><xhtml:link rel=\\"alternate\\" hreflang=\\"fr\\" href=\\"https://example.com/fr\\"/></url>
       </urlset>"
