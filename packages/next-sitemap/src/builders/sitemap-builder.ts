ccd52dcd67d31534ed842b20a005e8bd10e0f463
Jonathan <jhyahav2@gmail.com>
2022-10-18 18:32:31 +0300

    fix Google Search Console compatibility

--- packages/next-sitemap/src/builders/sitemap-builder.ts
+++ packages/next-sitemap/src/builders/sitemap-builder.ts
@@ -22,7 +22,7 @@ export class SitemapBuilder {
   buildSitemapIndexXml(allSitemaps: string[]) {
     return [
       '<?xml version="1.0" encoding="UTF-8"?>',
-      '<sitemapindex xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">',
+      '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
       ...(allSitemaps?.map((x) => `<sitemap><loc>${x}</loc></sitemap>`) ?? []),
       '</sitemapindex>',
     ].join('\n')
