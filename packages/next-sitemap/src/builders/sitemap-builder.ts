3eb4abfd5569934d9061cd6975cf1ee0c2c45b02
Mohammad Abdulhai <mohammad97adulhai@gmail.com>
2022-11-13 08:39:01 +0300

    Add https to fix issue when rendering sitemap that contain xhtml in chrome

--- packages/next-sitemap/src/builders/sitemap-builder.ts
+++ packages/next-sitemap/src/builders/sitemap-builder.ts
@@ -11,7 +11,7 @@ export class SitemapBuilder {
    * @returns
    */
   withXMLTemplate(content: string): string {
-    return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${content}</urlset>`
+    return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${content}</urlset>`
   }
 
   /**
