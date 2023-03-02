631794b9eea475c272240d00e248d4b87d976983
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2023-03-02 19:54:14 +0530

    Added app-dir sitemap support

--- examples/app-dir/app/(sitemaps)/server-sitemap.xml/route.ts
+++ examples/app-dir/app/(sitemaps)/server-sitemap.xml/route.ts
@@ -1,8 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getServerSideSitemap } from 'next-sitemap'
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!', {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/xml',
+  // Method to source urls from cms
+  // const urls = await fetch('https//example.com/api')
+
+  return getServerSideSitemap([
+    {
+      loc: 'https://example.com',
+      lastmod: new Date().toISOString(),
+      // changefreq
+      // priority
     },
-  })
+    {
+      loc: 'https://example.com/dynamic-path-2',
+      lastmod: new Date().toISOString(),
+      // changefreq
+      // priority
+    },
+  ])
 }
