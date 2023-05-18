85bcb66bdae316895b16d24a22c379d6326faafb
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2023-05-19 00:42:46 +0530

    Make output optional in readme

--- examples/static-export/next-sitemap.config.js
+++ examples/static-export/next-sitemap.config.js
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const nextConfig = require('./next.config')
-
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  ...nextConfig,
   siteUrl: process.env.SITE_URL || 'https://example.com',
   generateRobotsTxt: true,
+  output: 'static', // Set static output here
 }
 
 module.exports = config
