7d3e24b177bf3943723007955c62fccb6c2d326a
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-25 19:46:15 +0530

    Made the requested changes

---
+++ docs/app/components/Providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+import type { FC, ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
