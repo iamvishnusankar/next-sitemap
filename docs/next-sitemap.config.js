149e30a1d2dc64562d54c09423bc133fb960701a
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2026-03-11 01:59:23 +0530

    refactor(pipeline): Transition from Yarn to Bun for package management and update CI steps

--- docs/next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_APP_URL,
-  generateRobotsTxt: true,
-  output: 'export',
-}
--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { withContentlayer } = require('next-contentlayer')
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-}
-
-module.exports = withContentlayer(nextConfig)
